b2d723d56a509484c8612377a82280e839d31842
Muhammad Younas <younus.ubit56@gmail.com>
2017-05-27 16:27:35 +0500

    add miscl

---
+++ miscl.py
@@ -0,0 +1,17 @@
+
+
+class Token:
+
+    def __init__(self,CP,VP,LN):
+        self.CP=CP
+        self.VP=VP
+        self.LN=LN
+
+
+
+class matVariables:
+
+    def __init__(self,name,type,scope):
+        self.name=name
+        self.type=type
+        self.scope=scope
